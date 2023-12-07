493485c0e104f0df103ddff7dd31f9f10acfab33
NicolasLopezSTC <gclan149@gmail.com>
2023-12-06 18:52:28 -0600

    new final server
    Signed-off-by: NicolasLopezSTC <gclan149@gmail.com>

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
 import helmet from 'helmet'
-import bodyParser from 'body-parser'
-import basicAuth from 'express-basic-auth'
+
 import logger from './helpers/logger'
 import router from './routes'
 import { notFound, errorHandler } from './helpers/errors'
@@ -14,21 +13,9 @@ const port = Number(process.env.PORT)
 
 const app = express()
 
-app.use(
-  basicAuth({
-    users: { [process.env.ADMIN_USER]: process.env.ADMIN_PASSWORD },
-  }))
-
-
 app.use(morgan(process.env.MORGAN_LOG))
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    exposedHeaders: ['x-total-count', 'x-total-pages'],
-  }),
-)
+app.use(cors({ origin: process.env.CORS_ORIGIN }))
 app.use(helmet())
-app.use(bodyParser.json())
 
 app.use('/', router)
 
